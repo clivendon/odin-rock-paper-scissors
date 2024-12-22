28d599191fb3e344abb530158283b750cc308c3e
clivendon <clivendon1@gmail.com>
2024-12-21 21:12:29 -0500

    added functions for human and computer choices also working on the logic using if else statements to determine round winners

--- main.js
+++ main.js
@@ -1,13 +1,55 @@
-const getComputerChoice = () =>{
-let cpuChoice = Math.round(Math.random() * 3);
-if (cpuChoice === '1'){
-    return 'rock';
-}else if (cpuChoice === 2){
-    return 'paper';
-}else if (cpuChoice === 3){
-    return 'scissors';
-}else{
-    console.log('Error in getComputerChoice() function');
-}
+let humanScore = 0;
+let computerScore = 0;
+
+
+const getComputerChoice = () => {
+  let cpuChoice = Math.round(Math.random() * 3);
+  if (cpuChoice === "1") {
+    return "rock";
+  } else if (cpuChoice === 2) {
+    return "paper";
+  } else if (cpuChoice === 3) {
+    return "scissors";
+  } else {
+    console.log("Error in getComputerChoice() function");
+  }
+};
+const getHumanChoice = () => {
+  let choice = prompt("What's your choice? (Rock, Paper, or Scissors");
+  let choiceLowercase = choice.toLocaleLowerCase();
+
+  if (
+    choiceLowercase === "rock" ||
+    choiceLowercase === "paper" ||
+    choiceLowercase === "scissors"
+  ) {
+    return choiceLowercase;
+  } else {
+    console.log(
+      "You input an incorrect choice. Please select Rock Paper or Scissors"
+    );
+  }
 }
 
+const playRound = (getComputerChoice, getHumanChoice) => {
+    if (getComputerChoice === getHumanChoice){
+        return `It's a draw! The score is Player:${humanScore} to CPU:${computerScore}`
+    }else if (getHumanChoice === 'rock' && getComputerChoice === 'paper'){
+        computerScore++;
+        return `Paper beats Rock! Computer wins! The score is Player:${humanScore} to CPU:${computerScore}`
+    }else if (getHumanChoice === 'rock' && getComputerChoice === 'scissors'){
+        humanScoreScore++;
+        return `Rock beats Scissors! Player wins! The score is Player:${humanScore} to CPU:${computerScore}`
+    }else if (getHumanChoice === 'paper' && getComputerChoice === 'paper'){
+        computerScore++;
+        return `Paper beats Rock! Computer wins! The score is Player:${humanScore} to CPU:${computerScore}`
+    }else if (getHumanChoice === 'paper' && getComputerChoice === 'paper'){
+        computerScore++;
+        return `Paper beats Rock! Computer wins! The score is Player:${humanScore} to CPU:${computerScore}`
+    }else if (getHumanChoice === 'scissors' && getComputerChoice === 'paper'){
+        computerScore++;
+        return `Paper beats Rock! Computer wins! The score is Player:${humanScore} to CPU:${computerScore}`
+    }else if (getHumanChoice === 'scissors' && getComputerChoice === 'paper'){
+        computerScore++;
+        return `Paper beats Rock! Computer wins! The score is Player:${humanScore} to CPU:${computerScore}`
+    }
